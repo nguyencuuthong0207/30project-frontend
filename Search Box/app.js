d63853b9b5c1644c99e9a2d7789ce3d360535e68
cuuthong <nguyencuuthong0207>
2021-12-31 14:46:56 +0700

    add SearchBox

---
+++ Search Box/app.js
@@ -0,0 +1,7 @@
+openSearch = document.querySelector('.search')
+btnSearch = document.querySelector('.search__btn')
+
+btnSearch.addEventListener('click',function(){
+    this.parentElement.classList.toggle('open')
+    this.previousElementSibling.focus()
+})
