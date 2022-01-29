ceb45bd92a0cf6a825deff70b949a2132423137f
cuuthong <nguyencuuthong0207>
2022-01-29 21:52:05 +0700

    add navigation menu

---
+++ Navigation/app.js
@@ -0,0 +1,11 @@
+var list = document.querySelectorAll('.list');
+
+
+function activeList() {
+    list.forEach((item) => 
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click', activeList)
+)
